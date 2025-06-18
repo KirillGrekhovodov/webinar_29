7c4f69f13704175ef5787a1dc7cf2d41f1f68758
Kirill Grekhovodov <kirill@wildlabs.co>
2025-06-18 19:54:04 +0600

    create task4 view

--- webapp/views.py
+++ webapp/views.py
@@ -8,3 +8,7 @@ def index(request):
 
 def about(request):
     return render(request, "about.html")
+
+
+def task4(request):
+    return render(request, "task4.html")
--- webinar29/urls.py
+++ webinar29/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from webapp.views import index, about
+from webapp.views import index, about, task4
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-    path("about/", about)
+    path("about/", about),
+    path("task4/", task4)
 ]
